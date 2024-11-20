e2f3638c2cd175c895dbe5425716ed614c08fbe7
Justus Magin <keewis@posteo.de>
2024-11-20 22:38:05 +0100

    reimplement the formatter in python
    This avoids relying on `numpy` / `numpy` dtypes, so may make more sense?
    It might be slower, though, especially since every additional dimension means one
    additional recursion.

--- marray/formatting.py
+++ marray/formatting.py
@@ -1,3 +1,4 @@
+import math
 import re
 from functools import partial
 import textwrap
@@ -9,6 +10,73 @@ replace_re = re.compile(r"(?<=['\]])(?=\s|$)")
 
 def dedent(text):
     return textwrap.dedent(text.lstrip("\n").rstrip())
+
+
+def indent_by_one(text):
+    return textwrap.indent(text, " ").lstrip().rstrip()
+
+def format_item(item, masked):
+    return str(item) if not masked else "--"
+
+def format_array_1d(data, mask, max_items=20, visible=3):
+    if len(data) <= max_items or visible * 2 >= max_items:
+        result = ", ".join(
+            format_item(item, masked)
+            for item, masked in zip(data, mask)
+        )
+    else:
+        head = [
+            format_item(item, masked)
+            for item, masked in zip(data[:visible], mask[:visible])
+        ]
+        tail = [
+            format_item(item, masked)
+            for item, masked in zip(data[-visible:], mask[-visible:])
+        ]
+
+        result = ", ".join(head + ["..."] + tail)
+
+    return f"[{result}]"
+
+
+def format_array(data, mask, max_outer_items=10, max_inner_items=20, visible=3):
+    if data.ndim == 0:
+        return format_item(data, mask)
+    elif data.ndim == 1:
+        return format_array_1d(data, mask, max_items=max_inner_items, visible=visible)
+
+    if data.ndim == 1:
+        formatter = partial(
+            format_array_1d,
+            max_items=max_inner_items,
+            visible=visible,
+        )
+    else:
+        formatter = partial(
+            format_array,
+            max_outer_items=max_outer_items,
+            max_inner_items=max_inner_items,
+            visible=visible,
+        )
+
+    if data.shape[0] <= max_outer_items or visible * 2 >= max_outer_items:
+        result = ",\n".join(
+            formatter(data[index, ...], mask[index, ...])
+            for index in range(data.shape[0])
+        )
+    else:
+        head = [
+            formatter(data[index, ...], mask[index, ...])
+            for index in range(visible)
+        ]
+        tail = [
+            formatter(data[index, ...], mask[index, ...])
+            for index in range(-visible, 0)
+        ]
+
+        result = ",\n".join(head + ["..."] + tail)
+
+    return f"[{indent_by_one(result)}]"
 
 
 def format_data(data, mask):
