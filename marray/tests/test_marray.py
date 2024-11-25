945bcf93ff335631c1c193e7ad0f532f2c2fe00c
Matt Haberland <mhaberla@calpoly.edu>
2024-11-24 16:49:36 -0800

    TST: test manipulation functions

--- marray/tests/test_marray.py
+++ marray/tests/test_marray.py
@@ -13,13 +13,13 @@ dtypes_complex = ['complex64', 'complex128']
 dtypes_all = dtypes_boolean + dtypes_integral + dtypes_real + dtypes_complex
 
 
-def get_arrays(n_arrays, *, dtype='float64', xp=np, seed=None):
+def get_arrays(n_arrays, *, ndim=(1, 4), dtype='float64', xp=np, seed=None):
     xpm = marray.masked_array(xp)
 
     entropy = np.random.SeedSequence(seed).entropy
     rng = np.random.default_rng(entropy)
 
-    ndim = rng.integers(1, 4)
+    ndim = rng.integers(*ndim) if isinstance(ndim, tuple) else ndim
     shape = rng.integers(1, 20, size=ndim)
 
     datas = []
@@ -420,7 +420,53 @@ def test_statistical_array(f_name, keepdims, xp=np, dtype='float64', seed=None):
 # Data Type Functions (only `astype` remains to be tested)
 # Elementwise function `clip` (all others are tested above)
 # Indexing (same behavior as indexing data and mask separately)
-# Manipulation functions (apply to data and mask separately)
+
+
+@pytest.mark.parametrize('f_name, n_arrays, n_dims, kwargs', [
+    # Try to pass options that change output compared to default
+    ('broadcast_arrays', 3, (3, 5), dict()),
+    ('broadcast_to', 1, (3, 5), dict(shape=None)),
+    ('concat', 3, (3, 5), dict(axis=1)),
+    ('expand_dims', 1, (3, 5), dict(axis=1)),
+    ('flip', 1, (3, 5), dict(axis=1)),
+    ('moveaxis', 1, (3, 5), dict(source=1, destination=2)),
+    ('permute_dims', 1, 3, dict(axes=[2, 0, 1])),
+    ('repeat', 1, (3, 5), dict(repeats=2, axis=1)),
+    ('reshape', 1, (3, 5), dict(shape=(-1,), copy=False)),
+    ('roll', 1, (3, 5), dict(shift=3, axis=1)),
+    ('squeeze', 1, (3, 5), dict(axis=1)),
+    ('stack', 3, (3, 5), dict(axis=1)),
+    ('tile', 1, (3, 5), dict(reps=(2, 3))),
+    ('unstack', 1, (3, 5), dict(axis=1)),
+])
+def test_creation(f_name, n_arrays, n_dims, kwargs, seed=None, xp=np):
+    mxp = marray.masked_array(xp)
+    marrays, _, seed = get_arrays(n_arrays, ndim=n_dims, dtype=xp.float64, seed=seed)
+    if f_name in {'broadcast_to', 'squeeze'}:
+        original_shape = marrays[0].shape
+        marrays[0] = marrays[0][:, 0:1, ...]
+        if f_name == "broadcast_to":
+            kwargs['shape'] = original_shape
+
+    f_mxp = getattr(mxp, f_name)
+    f_xp = getattr(xp, f_name)
+
+    if f_name in {'concat', 'stack'}:
+        marrays = mxp.broadcast_arrays(*marrays)
+        res = (f_mxp(marrays, **kwargs))
+        ref_data = f_xp([marray.data for marray in marrays], **kwargs)
+        ref_mask = f_xp([marray.mask for marray in marrays], **kwargs)
+    else:
+        res = f_mxp(*marrays, **kwargs)
+        ref_data = f_xp(*[marray.data for marray in marrays], **kwargs)
+        ref_mask = f_xp(*[marray.mask for marray in marrays], **kwargs)
+
+    ref = np.ma.masked_array(ref_data, mask=ref_mask)
+
+    if f_name in {'broadcast_arrays', 'unstack'}:
+        [assert_equal(res_i, ref_i, seed) for res_i, ref_i in zip(res, ref)]
+    else:
+        assert_equal(res, ref, seed)
 
 #?
 # Searching functions - would test argmin/argmax with statistical functions,
