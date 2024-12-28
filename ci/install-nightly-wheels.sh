2136530b6add74a7cccc2ea906b908e00a95a9ee
Lucas Colley <lucas.colley8@gmail.com>
2024-12-28 16:31:06 +0000

    use pixi for nightly CI (#52)

--- ci/install-nightly-wheels.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python -m pip uninstall numpy
-
-python -m pip install --upgrade --no-deps --pre \
-    -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \
-    numpy
